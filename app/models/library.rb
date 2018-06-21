d4f738a82e8c9aba3a3044ae865ecf129ee6e0f4
JohnZBurnett <tuninator@gmail.com>
2018-06-21 11:47:54 -0400

    add analytics

--- app/models/library.rb
+++ app/models/library.rb
@@ -7,6 +7,7 @@ class Library < ApplicationRecord
     CheckedOutBook.checkouts_by_library(self).count
   end
 
+
   def book_copies_count
     self.book_copies.count
   end
@@ -30,5 +31,23 @@ class Library < ApplicationRecord
 
   def percentage_requests_incomplete
     (self.requests_by_library - self.completed_requests_count).to_f / requests_by_library.to_f
+  end
+
+  def self.most_checkouts
+    Library.all.sort do |library1, library2|
+      library1.checkout_count <=> library2.checkout_count
+    end.last
+  end
+
+  def self.sort_by_requests
+    Library.all.sort do |library1, library2|
+      library1.requests_by_library <=> library2.requests_by_library
+    end
+  end
+
+  def self.most_book_copies
+    Library.all.sort do |library1, library2|
+      library1.book_copies_count <=> library2.book_copies_count
+    end.last
   end
 end
